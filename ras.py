4b61ca96cbf5c40f3d77b577d81eae0c4788c0c2
mahjoubmahjoub <mahjoub.m@shazler.com>
2025-05-26 21:14:01 +0100

    26/5

--- ras.py
+++ ras.py
@@ -1,12 +1,18 @@
 import cv2
 
 cap = cv2.VideoCapture(0)
+
 if not cap.isOpened():
     print("Impossible d'ouvrir la caméra")
+    exit()
+
+ret, frame = cap.read()
+if ret:
+    print("Image capturée OK")
+    cv2.imshow("Test", frame)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
 else:
-    ret, frame = cap.read()
-    if ret:
-        print("Image capturée OK")
-    else:
-        print("Impossible de lire une image")
+    print("Impossible de lire une image")
+
 cap.release()

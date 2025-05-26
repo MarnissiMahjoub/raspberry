84d85f235739d0b49d4363b3458f137ce7bb0f1d
mahjoubmahjoub <mahjoub.m@shazler.com>
2025-05-26 23:05:17 +0100

    26/5

--- ras.py
+++ ras.py
@@ -4,7 +4,7 @@ from picamera2 import Picamera2
 
 # Initialisation caméra
 picam2 = Picamera2()
-picam2.preview_configuration.main.size = (640, 480)
+picam2.preview_configuration.main.size = (3280, 2464)  # Résolution max pour IMX219
 picam2.preview_configuration.main.format = "RGB888"
 picam2.configure("preview")
 picam2.start()

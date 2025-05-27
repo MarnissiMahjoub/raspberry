6cf91ec03a6030947eb0d7f5bfe22b742c098df6
mahjoubmahjoub <mahjoub.m@shazler.com>
2025-05-27 04:17:05 +0100

    27/5

--- ras.py
+++ ras.py
@@ -9,15 +9,17 @@ time.sleep(2)  # Laisse le temps à la caméra de démarrer
 frame_ref = picam2.capture_array()
 frame_ref_gray = cv2.cvtColor(frame_ref, cv2.COLOR_BGR2GRAY)
 
+counter = 0
+
 while True:
     frame = picam2.capture_array()
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     diff = cv2.absdiff(frame_ref_gray, gray)
     _, thresh = cv2.threshold(diff, 30, 255, cv2.THRESH_BINARY)
 
-    cv2.imshow('Diff', thresh)
+    filename = f"diff_{counter}.jpg"
+    cv2.imwrite(filename, thresh)
+    print(f"Image sauvegardée : {filename}")
+    counter += 1
 
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
-
-cv2.destroyAllWindows()
+    time.sleep(1)  # Capture toutes les secondes
